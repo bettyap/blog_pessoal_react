afc4100dbd64bac7a25349d39d7c722fb89c9fb0
Elisabeth Aparecida <beth_ap@live.com>
2024-01-21 07:41:18 -0300

    feat: Add page Cadastro and login, create services and implementation Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import NavBar from './components/navbar/NavBar'
 import Home from './pages/home/Home'
+import Cadastro from './pages/cadastro/Cadastro'
+import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
   return (
     <>
-      <NavBar/>
-      <Home/>
-      <Footer/>
+      <AuthProvider>
+        <BrowserRouter>
+          <NavBar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
