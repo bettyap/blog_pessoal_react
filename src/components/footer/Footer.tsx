e8c4497d7258880bbebde5a90a04c4ae0911687a
Elisabeth Aparecida <beth_ap@live.com>
2024-01-30 12:27:32 -0300

    feat: ajuste na navbar e footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,28 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
+
+  let data = new Date().getFullYear()
+
   return (
     <>
-      <div className="flex justify-center bg-indigo-900 text-white">
+      <div className="flex justify-center bg-dodger-blue-700 text-white">
         <div className="container flex flex-col items-center py-4">
-          <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: </p>
+          <p className='text-xl font-bold'>Blog pessoal Generation | Copyright: {data}</p>
           <p className='text-lg'>Acesse nossas redes sociais</p>
           <div className='flex gap-2'>
-            <LinkedinLogo size={48} weight='bold' />
-            <InstagramLogo size={48} weight='bold' />
-            <FacebookLogo size={48} weight='bold' />
+            <a href="https://github.com/bettyap">
+              <GithubLogo size={48} weight='bold' className='hover:text-dodger-blue-500'/>
+            </a>
+            <a href="https://www.linkedin.com/in/elisabeth-aparecida/">
+              <LinkedinLogo size={48} weight='bold' className='hover:text-dodger-blue-500'/>
+            </a>
+            <a href="https://www.instagram.com/betty_apareciida/">
+              <InstagramLogo size={48} weight='bold' className='hover:text-dodger-blue-500'/>
+            </a>
+            <a href="https://www.facebook.com/bettyapareciida/">
+              <FacebookLogo size={48} weight='bold' className='hover:text-dodger-blue-500'/>
+            </a>
           </div>
         </div>
       </div>
--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,33 +1,42 @@
-import { useContext } from "react";
+import { ReactNode, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom"
 import { AuthContext } from "../../contexts/AuthContext";
+import { ToastAlerta } from "../../utils/ToastAlerts";
 
 function NavBar() {
 
   const navigate = useNavigate();
 
-  const { handleLogout } = useContext(AuthContext)
+  const { usuario, handleLogout } = useContext(AuthContext)
 
   function logout() {
     handleLogout()
-    alert('O Usuário foi desconectado com sucesso!')
+    ToastAlerta('O Usuário foi desconectado com sucesso!', 'sucesso')
     navigate('/login')
+  }
+
+  let navbarComponent: ReactNode
+
+  if (usuario.token !== "") {
+    navbarComponent = (
+      <div className='w-full bg-dodger-blue-700  text-white flex justify-center py-4'>
+        <div className="container flex justify-between text-lg">
+          <Link to='/' className="text-2xl font-bold">Blog Pessoal</Link>
+          <div className='flex gap-4'>
+            <Link to='/postagens' className='hover:text-dodger-blue-500'>Postagens</Link>
+            <Link to='/temas' className='hover:text-dodger-blue-500'>Temas</Link>
+            <Link to='/cadastroTema' className='hover:text-dodger-blue-500'>Cadastrar tema</Link>
+            <Link to='/perfil' className='hover:text-dodger-blue-500'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:text-dodger-blue-500 hover:underline'>Sair</Link>
+          </div>
+        </div>
+      </div>
+    )
   }
 
   return (
     <>
-      <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-        <div className="container flex justify-between text-lg">
-          <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
-          <div className='flex gap-4'>
-            Postagens
-            <Link to='/temas'>Temas</Link>
-            <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
-            Perfil
-            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
-          </div>
-        </div>
-      </div>
+      {navbarComponent}
     </>
   )
 }
