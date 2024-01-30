09f8893b6e834d58dbad7193fe61762781654fb2
Elisabeth Aparecida <beth_ap@live.com>
2024-01-30 12:28:40 -0300

    feat: implementado o CRUD da Postagem e realizado alguns ajustes

---
+++ src/components/postagens/cardPostagem/CardPostagem.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom"
+import Postagem from "../../../models/Postagem"
+
+interface cardPostagemProps {
+  post: Postagem
+}
+
+function CardPostagem({ post }: cardPostagemProps) {
+  return (
+    <div className="border-slate-900 border flex flex-col rounded overflow-hidden justify-between">
+      <div>
+        <div className="flex w-full bg-dodger-blue-600 py-2 px-4 items-center gap-4">
+          <img src={post.usuario?.foto} className="h-12 rounded-full" alt="" />
+          <h3 className="text-lg font-bold text-center uppercase">{post.usuario?.nome}</h3>
+        </div>
+        <div className="p-4">
+          <h4 className="text-lg font-semibold uppercase">{post.titulo}</h4>
+          <p>{post.texto}</p>
+          <p>{post.tema?.descricao}</p>
+          <p>{new Intl.DateTimeFormat(undefined, {
+            dateStyle: 'full',
+            timeStyle: 'medium',
+          }).format(new Date(post.data))}</p>
+        </div>
+      </div>
+      <div className="flex">
+        <Link to={`/editarPostagem/${post.id}`} className='w-full text-white bg-dodger-blue-500 hover:bg-dodger-blue-600 flex items-center justify-center py-2'>
+          <button>Editar</button>
+        </Link>
+        <Link to={`/deletarPostagem/${post.id}`} className='text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center'>
+          <button>Deletar</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default CardPostagem
