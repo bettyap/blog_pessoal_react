acd653ca45a5156b6149ab642a86f848402dcbda
Elisabeth Aparecida <beth_ap@live.com>
2024-01-30 12:26:39 -0300

    feat: configuração do tailwind, toastify

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,19 @@ import { AuthProvider } from './contexts/AuthContext'
 import ListaTemas from './components/temas/listaTemas/ListaTemas'
 import FormularioTema from './components/temas/formularioTema.tsx/FormularioTema'
 import DeletarTema from './components/temas/deletarTema/DeletarTema'
+import ListaPostagem from './components/postagens/listaPostagens/cardPostagens/ListaPostagem'
+import FormularioPostagem from './components/postagens/formularioPostagem/FormularioPostagem'
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem'
+import Perfil from './pages/perfil/Perfil'
+
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <AuthProvider>
+        <ToastContainer />
         <BrowserRouter>
           <NavBar />
           <div className='min-h-[80vh]'>
@@ -25,6 +33,11 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagem />} />
+              <Route path="/cadastroPostagem" element={<FormularioPostagem />} />
+              <Route path="/editarPostagem/:id" element={<FormularioPostagem />} />
+              <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dodger-blue': {
+          '50': '#f0f7ff',
+          '100': '#e0effe',
+          '200': '#bbdefc',
+          '300': '#7fc4fa',
+          '400': '#3aa6f6',
+          '500': '#118de9',
+          '600': '#056dc4',
+          '700': '#05569f',
+          '800': '#094b83',
+          '900': '#0d3f6d',
+          '950': '#092848',
+        },
+      }
+    },
   },
   plugins: [],
 }
